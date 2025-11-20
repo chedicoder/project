911b7c5e0c1b6d583790be1333f65fbd199fd846
chedi <chedi@example.com>
2025-11-20 15:21:58 +0100

    delete cert repo

--- cert/src/main/java/com/example/cert/CertApplication.java
+++ cert/src/main/java/com/example/cert/CertApplication.java
@@ -1,13 +1,13 @@
-package com.example.cert_client;
+package com.example.cert;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CertClientApplication {
+public class CertApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CertClientApplication.class, args);
+		SpringApplication.run(CertApplication.class, args);
 	}
 
 }
